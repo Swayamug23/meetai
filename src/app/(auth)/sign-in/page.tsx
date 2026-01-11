8b710bbc60c257fd3f86739a3166cd0983b62a46
Swayamug23 <swayam.ug23@nsut.ac.in>
2026-01-11 21:22:52 +0530

    Authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/components/ui/card'
+import SignInView from '@/modules/auth/ui/views/SignInView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignInView/>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-     
-    <Button> hey im the btn</Button>
-    </>
-  );
-}
+
+  const {
+    data: session,
+  } = authClient.useSession();
+
+  if (session)
+    return (
+      <>
+        <div>
+          logged in as {session.user.name}
+        </div>
+        <Button onClick={() => {
+          authClient.signOut();
+        }}>
+          Sign Out
+        </Button>
+      </>
+    );
+
+    return(
+      <>
+      <div>
+        Hello world</div>
+        <Link href="/sign-in">Sign In</Link>
+      </>
+    )}
+    
