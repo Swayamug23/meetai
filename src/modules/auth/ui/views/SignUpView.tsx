65e1c36e9d4d3f3309bc1f727945948f18e2149e
Swayamug23 <swayam.ug23@nsut.ac.in>
2026-01-18 21:15:48 +0530

    Dashboard sidebar

--- src/modules/auth/ui/views/SignUpView.tsx
+++ src/modules/auth/ui/views/SignUpView.tsx
@@ -215,7 +215,7 @@ const SignUpView = () => {
 
 
 
-          <div className='bg-radial p-2 from-green-500 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center'>
+          <div className='bg-radial p-2 from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center'>
 
             <img src="./logo.svg" alt="image" className='h-[90px] w-[90px]' />
             <p className='text-2xl font-bold text-white'>
